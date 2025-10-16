8b75e03091d8169b09bed8813cc216b7f18af334
Md. Sahin Alan <sahinraj20@gmail.com>
2025-10-16 21:56:21 +0600

    Updated router in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import authRouter from './modules/auth/auth.routes';
 import userRouter from './modules/user/user.routes';
+import jobRouter from './modules/job/job.routes';
+import applicationRouter from './modules/application/application.routes';
+import organizationRouter from './modules/organization/organization.routes';
 
 const app = express();
 app.use(cors());
@@ -10,6 +13,9 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+app.use('/api/job', jobRouter);
+app.use('/api/application', applicationRouter);
+app.use('/api/organization', organizationRouter);
 
 
 
